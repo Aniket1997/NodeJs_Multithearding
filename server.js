9b994250f609307303ad56ce6bd10f1cf7e68cfa
Aniket1997 <16riju@gmail.com>
2025-01-09 12:08:03 +0530

    Initiate the problem

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.get('/non-blocking',(req, res) => {
+    try {
+        return res.status(200).json({ status: 'Successful', message:'Hello world' });
+    } catch (error) {
+        return res.status(500).json({ status: 'Failed', message: 'Error occurred' });
+    }
+});
+
+
+app.get('/blocking', async (req, res) => {
+    try {
+        let counter = 0;
+        for(let i=0;i<2000_0000_0000_0000;i++)
+        {
+            counter++;
+        }
+        return res.status(200).json({ status: 'Success', message: 'Success', counter });
+    } catch (error) {
+        return res.status(500).json({ status: 'Failed', message: 'Internal Server Error' });
+    }
+});
 
 
 app.listen(8000,()=>{
